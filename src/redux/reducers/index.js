7879441f00d9f8af8a00cc69de8880e197a6c50d
StephenReiser <Stephen.Reiser1@gmail.com>
2019-08-29 21:11:27 -0400

    removed stuff

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-// this is the rootreducer file
-
-import { combineReducers } from 'redux';
-import footballReducer from './footballReducer'
-
-
-export default combineReducers({
-    football: footballReducer
-})
